f2ad3c86fadc7433530ed9d612a7b07e58d4dd21
RyanSamman <RyanSamman@outlook.com>
2020-05-25 19:33:25 +0300

    Initial commit

---
+++ ArrayMethods/arraymethods.js
@@ -0,0 +1,17 @@
+// forEach
+// Executes the function on every index of the array, like a for loop
+
+// map
+// Creates new array from old one, modified with return of function on that index
+
+// * reduce
+// from an initial position, it returns a "Reduced" variable 
+
+// filter
+// Whether the result of the function is true or false, will add the items
+
+// every
+// Tests if all items return true 
+
+// some
+// Tests if one or more return true
